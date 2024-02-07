9babf3f0e97e92dc7d39fd790071fe9883e231b2
HannibalSnekter <alexhayes1206@googlemail.com>
2024-02-07 13:37:36 +0000

    Update HomeTransition.cs
    - Fixed a bug with Chimeras fight skip not triggering after loading a save from the hallway save sphere
    - Fixed a bug with Chimeras fight being skippable by opening the menu after going into the Chimera side room

--- FFXCutsceneRemover/Components/HomeTransition.cs
+++ FFXCutsceneRemover/Components/HomeTransition.cs
@@ -1,10 +1,21 @@
-﻿namespace FFXCutsceneRemover;
+﻿using FFXCutsceneRemover.Logging;
+
+namespace FFXCutsceneRemover;
 
 class HomeTransition : Transition
 {
     public override void Execute(string defaultDescription = "")
     {
-        if (Stage == 0 && MemoryWatchers.CutsceneAlt.Current == 5043)
+        // If currently in the main hallway and Stage != 3, i.e went in side room or loaded from a save in hallway, get Event File address and set Stage
+        if (Stage != 3 && MemoryWatchers.MovementLock.Current == 0x20 && MemoryWatchers.RoomNumber.Current == 280 && MemoryWatchers.Storyline.Current == 1885)
+        {
+            base.Execute();
+            BaseCutsceneValue = MemoryWatchers.EventFileStart.Current;
+            Stage = 3;
+        }
+
+        // Get Event File Address Data
+        if (Stage == 0 && MemoryWatchers.CutsceneAlt.Current == 5043 && MemoryWatchers.Storyline.Current == 1820)
         {
             base.Execute();
 
@@ -12,20 +23,25 @@ class HomeTransition : Transition
             Stage += 1;
 
         }
+        // Skip cutscene after walking through door post bombs
         else if (MemoryWatchers.HomeTransition.Current == (BaseCutsceneValue + 0x5FDB) && Stage == 1)
         {
             WriteValue<int>(MemoryWatchers.HomeTransition, BaseCutsceneValue + 0x61CE);
             Stage += 1;
         }
+        // Set camera lock value after defeating Dual Horns so the camera faces up the stairs
         else if (MemoryWatchers.BattleState2.Current == 1 && Stage == 2)
         {
             WriteValue<int>(MemoryWatchers.Camera, 0);
             Stage += 1;
         }
+        // Skip cutscene before the Chimeras encounter
         else if (MemoryWatchers.HomeTransition.Current == (BaseCutsceneValue + 0x63AA) && Stage == 3)
         {
+            // Remove cutscene timing to remove low fps
             WriteValue<byte>(MemoryWatchers.CutsceneTiming, 0);
 
+            // Trigger Chimeras encounter
             new Transition
             {
                 EncounterMapID = 87,
@@ -43,14 +59,15 @@ class HomeTransition : Transition
             Stage += 1;
         }
 
-        if (MemoryWatchers.RoomNumber.Current == 275 || MemoryWatchers.RoomNumber.Current == 286)
+        // If going into a side room set a bogus Stage value until returning to main hallway
+        if ((MemoryWatchers.RoomNumber.Current == 275 || MemoryWatchers.RoomNumber.Current == 286) && Stage != 99)
         {
             Stage = 99;
         }
-        else if (MemoryWatchers.RoomNumber.Current == 280 && Stage == 99)
+        // Clean Encounter Data after battle in a side room
+        else if (MemoryWatchers.EncounterMapID.Current != 0 && MemoryWatchers.BattleState2.Current == 1 && Stage == 99)
         {
-            BaseCutsceneValue = MemoryWatchers.EventFileStart.Current;
-            Stage = 3;
+            new Transition { EncounterMapID = 0, EncounterFormationID1 = 0, EncounterFormationID2 = 0, ForceLoad = false, /*ConsoleOutput = false,*/ Description = "Clean Encounter Data" }.Execute();
         }
     }
 }
